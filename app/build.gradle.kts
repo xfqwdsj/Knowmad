8a3350f1795a9757bfa1189d6d3c604817eb3e20
LTFan <xfqwdsj@qq.com>
2025-12-06 11:04:53 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+/*
+ * Knowmad - Knowledge nomad
+ * Copyright (C) 2025 LTFan (aka xfqwdsj)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "top.ltfan.knowmad"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "top.ltfan.knowmad"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+}
