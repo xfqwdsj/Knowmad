15a2916de7d0c51a7ec39612aac1697f402c37f3
LTFan <xfqwdsj@qq.com>
2025-12-16 16:36:25 +0800

    refactor: make expanded property to return a List<Page>

--- app/src/main/kotlin/top/ltfan/knowmad/ui/page/Page.kt
+++ app/src/main/kotlin/top/ltfan/knowmad/ui/page/Page.kt
@@ -20,8 +20,6 @@ package top.ltfan.knowmad.ui.page
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.toMutableStateList
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
@@ -60,12 +58,12 @@ abstract class BackStackRoute(
     val backStack: NavBackStack<Route>,
 ) : Route()
 
-val NavBackStack<Route>.expanded: SnapshotStateList<Page>
-    @Composable get() = buildList {
+val NavBackStack<Route>.expanded: List<Page>
+    get() = buildList {
         for (entry in this@expanded) {
             when (entry) {
                 is Page -> add(entry)
                 is BackStackRoute -> addAll(entry.backStack.expanded)
             }
         }
-    }.toMutableStateList()
+    }
