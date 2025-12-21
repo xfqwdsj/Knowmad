ad1b1bb7e33b7ed643c8d9fd4f0fcfcfeb17c8fc
LTFan <xfqwdsj@qq.com>
2025-12-21 18:06:33 +0800

    feat: refactor Markdown component to improve state management and enhance composability

--- app/src/main/kotlin/top/ltfan/knowmad/ui/component/Markdown.kt
+++ app/src/main/kotlin/top/ltfan/knowmad/ui/component/Markdown.kt
@@ -19,22 +19,88 @@
 package top.ltfan.knowmad.ui.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mikepenz.markdown.compose.components.markdownComponents
 import com.mikepenz.markdown.m3.Markdown
+import com.mikepenz.markdown.model.MarkdownState
+import com.mikepenz.markdown.model.State
+import com.mikepenz.markdown.model.parseMarkdownFlow
 import com.mikepenz.markdown.model.rememberMarkdownState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
+import kotlin.random.Random
 
 @Composable
-fun Markdown(
+fun MarkdownView(
     markdown: String,
     modifier: Modifier = Modifier,
 ) {
-    val state = rememberMarkdownState(markdown, retainState = true)
+    val markdownState = rememberMarkdownState(markdown, retainState = true)
+    MarkdownView(
+        markdownState,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun MarkdownView(
+    markdownState: MarkdownState,
+    modifier: Modifier = Modifier,
+) {
+    val state by markdownState.state.collectAsState()
+    MarkdownView(
+        state,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun MarkdownView(
+    state: State,
+    modifier: Modifier = Modifier,
+) {
     Markdown(
         state,
         modifier = modifier,
         components = markdownComponents { type, model ->
 
         },
+    )
+}
+
+private class MarkdownViewModel(markdownFlow: Flow<String>) : ViewModel() {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val markdownState = markdownFlow
+        .flatMapLatest { parseMarkdownFlow(it) }
+        .filter { it !is State.Loading }
+        .flowOn(Dispatchers.Default)
+        .stateIn(viewModelScope, SharingStarted.Eagerly, State.Loading())
+}
+
+@Composable
+fun MarkdownView(
+    markdownFlow: Flow<String>,
+    modifier: Modifier = Modifier,
+    key: String = rememberSaveable { Random.nextLong().toString() },
+) {
+    val viewModel = viewModel(key = key) { MarkdownViewModel(markdownFlow) }
+    val markdownState by viewModel.markdownState.collectAsStateWithLifecycle()
+
+    MarkdownView(
+        markdownState,
+        modifier = modifier,
     )
 }
