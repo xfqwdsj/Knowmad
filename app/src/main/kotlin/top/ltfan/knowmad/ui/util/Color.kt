7cb3eb7f64c4e4ea5a13b6d7a6c3400e37a2e17f
LTFan <xfqwdsj@qq.com>
2026-01-24 19:12:31 +0800

    feat: add utility function for determining contract color based on background luminance

---
+++ app/src/main/kotlin/top/ltfan/knowmad/ui/util/Color.kt
@@ -0,0 +1,28 @@
+/*
+ * Knowmad - Knowledge nomad
+ * Copyright (C) 2026 LTFan (aka xfqwdsj)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package top.ltfan.knowmad.ui.util
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+
+fun contractColorFor(
+    backgroundColor: Color,
+    lightColor: Color = Color.White,
+    darkColor: Color = Color.Black,
+) = if (backgroundColor.luminance() > 0.5f) darkColor else lightColor
