aba9c2f16cd49add8dc9b3c40e62dfafb482b0c9
LTFan <xfqwdsj@qq.com>
2026-01-26 19:27:49 +0800

    chore: change app version to 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
     }
 
     val keyStoreFile = file("key.jks")
